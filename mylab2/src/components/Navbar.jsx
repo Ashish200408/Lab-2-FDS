02bfe3e4fbcfc4cec7d0fab6323f105a2062dbf2
Ashish Mishra <ashish.aashu2004@gmail.com>
2026-02-07 11:24:10 +0530

    Done

--- mylab2/src/components/Navbar.jsx
+++ mylab2/src/components/Navbar.jsx
@@ -1,11 +1,29 @@
-import React from 'react'; 
-import { Link } from 'react-router-dom';
-function Navbar() { 
-    return ( <nav style={{ padding: '10px', background: '#f0f0f0', textAlign: 'center' }}> 
-    {/* We use Link instead of <a href> to prevent page refresh */}
-<Link to="/" style={{ marginRight: '15px' }}>Home</Link> 
-<Link to="/about">About</Link> 
-</nav> 
-); 
-} 
+import React from "react";
+import { Link } from "react-router-dom";
+
+function Navbar() {
+  return (
+    <nav
+      style={{
+        padding: "10px",
+        background: "#f0f0f0",
+        textAlign: "center"
+      }}
+    >
+      <Link to="/" style={{ marginRight: "15px" }}>
+        Home
+      </Link>
+
+      <Link to="/about" style={{ marginRight: "15px" }}>
+        About
+      </Link>
+
+      {/* Contact shown in navbar only */}
+      <span style={{ color: "blue", cursor: "default" }}>
+        Contact
+      </span>
+    </nav>
+  );
+}
+
 export default Navbar;
